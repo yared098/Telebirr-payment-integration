e67f8b2eee984e59b7dab720bd086e91fcb711db
yared098 <fdessalew@gmail.com>
2026-04-27 13:44:48 +0300

    3rd paymnet phase and done test mode

--- service/createOrderService.js
+++ service/createOrderService.js
@@ -3,24 +3,6 @@ const tools = require("../utils/tools");
 const axios = require("axios");
 const config = require("../config/config");
 
-// exports.createOrder = async (req, res) => {
-//   let title = req.body.title;
-//   let amount = req.body.amount;
-//   let applyFabricTokenResult = await applyFabricToken();
-//   let fabricToken = applyFabricTokenResult.token;
-//   console.log("fabricToken =", fabricToken);
-//   let createOrderResult = await exports.requestCreateOrder(
-//     fabricToken,
-//     title,
-//     amount
-//   );
-//   console.log(createOrderResult);
-//   let prepayId = createOrderResult.biz_content.prepay_id;
-//   let rawRequest = createRawRequest(prepayId);
-//   console.log("RAW_REQ: ", rawRequest);
-//   res.send(rawRequest);
-//   return rawRequest;
-// };
 
 exports.createOrder = async (req, res) => {
   try {
@@ -69,30 +51,7 @@ exports.createOrder = async (req, res) => {
   }
 };
 
-// exports.requestCreateOrder = async (fabricToken, title, amount) => {
-//   try {
-//     const reqObject = createRequestObject(title, amount);
-//     console.log(reqObject);
 
-//     const response = await axios.post(
-//       `${config.baseUrl}/payment/v1/merchant/preOrder`,
-//       reqObject,
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//           "X-APP-Key": config.fabricAppId,
-//           Authorization: fabricToken,
-//         },
-//       }
-//     );
-
-//     // Assuming your response is a JSON object, no need to parse it
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error while requesting create order:", error.message);
-//     throw error; // Propagate the error for handling at a higher level
-//   }
-// };  
 exports.requestCreateOrder = async (fabricToken, title, amount) => {
   try {
     const reqObject = createRequestObject(title, amount);
@@ -116,34 +75,6 @@ exports.requestCreateOrder = async (fabricToken, title, amount) => {
   }
 };
 
-// function createRequestObject(title, amount) {
-//   let req = {
-//     timestamp: tools.createTimeStamp(),
-//     nonce_str: tools.createNonceStr(),
-//     method: "payment.preorder",
-//     version: "1.0",
-//   };
-//   let biz = {
-//     // notify_url: "https://node-api-muxu.onrender.com/api/v1/notify",
-//     trade_type: "InApp",
-//     appid: config.merchantAppId,
-//     merch_code: config.merchantCode,
-//     merch_order_id: createMerchantOrderId(),
-//     title: "Game1",
-//     total_amount: "150",
-//     trans_currency: "ETB",
-//     timeout_express: "120m",
-//     payee_identifier: config.merchantCode,
-//     payee_identifier_type: "04",
-//     payee_type: "5000",
-//     // redirect_url: "https://216.24.57.253/api/v1/notify",
-//   };
-//   req.biz_content = biz;
-//   req.sign = tools.signRequestObject(req);
-//   req.sign_type = "SHA256WithRSA";
-//   console.log(req);
-//   return req;
-// }
 
 function createRequestObject(title, amount) {
   let req = {
@@ -154,7 +85,7 @@ function createRequestObject(title, amount) {
   };
 
   let biz = {
-    trade_type: "InApp",
+    trade_type: "Checkout",
     appid: config.merchantAppId,
     merch_code: config.merchantCode,
     merch_order_id: createMerchantOrderId(),
